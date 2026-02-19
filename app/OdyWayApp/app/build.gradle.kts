a4c0428f1782e3b892d65896447376aa48f315e2
XavierGiralt <xavier@xgiralt.com>
2026-02-19 14:10:37 +0100

    Logo correcte

--- app/OdyWayApp/app/build.gradle.kts
+++ app/OdyWayApp/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation(libs.androidx.core.splashscreen)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
