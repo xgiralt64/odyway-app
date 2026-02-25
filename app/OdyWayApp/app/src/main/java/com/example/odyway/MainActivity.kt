42fdc5295a67b620b5bcae5507811c95b964a023
XavierGiralt <xavier@xgiralt.com>
2026-02-25 11:54:43 +0100

    Afegida splash provisional i home

--- app/OdyWayApp/app/src/main/java/com/example/odyway/MainActivity.kt
+++ app/OdyWayApp/app/src/main/java/com/example/odyway/MainActivity.kt
@@ -4,44 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.odyway.ui.theme.OdyWayTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             OdyWayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                NavGraph()   //Carreguem la navegació de moment
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OdyWayTheme {
-        Greeting("Android")
     }
 }
--- app/OdyWayApp/app/src/main/java/com/example/odyway/ui/screens/SplashScreen.kt
+++ app/OdyWayApp/app/src/main/java/com/example/odyway/ui/screens/SplashScreen.kt
@@ -1,4 +1,31 @@
 package com.example.odyway.ui.screens
 
-class SplashScreen {
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+//Splash screen de prova. Falta modificar
+
+@Composable
+fun SplashScreen(
+    onNavigateToHome: () -> Unit
+) {
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onNavigateToHome()
+    }
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "OdyWay",
+            style = MaterialTheme.typography.headlineLarge
+        )
+    }
 }
