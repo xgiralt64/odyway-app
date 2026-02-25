ade0c8f3420651bedfb69bcb24caba9d31056c28
XavierGiralt <xavier@xgiralt.com>
2026-02-25 12:19:30 +0100

    Afegida splash provisional i home

--- app/OdyWayApp/app/build.gradle.kts
+++ app/OdyWayApp/app/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
--- app/OdyWayApp/app/src/main/java/com/example/odyway/ui/screens/SplashScreen.kt
+++ app/OdyWayApp/app/src/main/java/com/example/odyway/ui/screens/SplashScreen.kt
@@ -1,14 +1,19 @@
 package com.example.odyway.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.odyway.R
 import kotlinx.coroutines.delay
-
-//Splash screen de prova. Falta modificar
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun SplashScreen(
@@ -20,12 +25,39 @@ fun SplashScreen(
     }
 
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "OdyWay",
-            style = MaterialTheme.typography.headlineLarge
-        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            // Logo
+            Image(                                      //CANVIAR PER EL LOGO REAL
+                painter = painterResource(id = R.drawable.splash),
+                contentDescription = "Logo OdyWay",
+                modifier = Modifier
+                    .size(180.dp)
+            )
+
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            // Loading
+            CircularProgressIndicator(
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Text(
+                text = "Carregant...",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
