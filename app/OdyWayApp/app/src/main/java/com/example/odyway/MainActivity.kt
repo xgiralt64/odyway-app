b9a1a02e6db4a55f2baf8af4ebafae21718600e8
ben Jonas <benjonas672@gmail.com>
2026-03-03 22:19:12 +0100

    barra de navegacion y homescreen

--- app/OdyWayApp/app/src/main/java/com/example/odyway/MainActivity.kt
+++ app/OdyWayApp/app/src/main/java/com/example/odyway/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.odyway.ui.theme.OdyWayTheme
 
 class MainActivity : ComponentActivity() {
--- app/OdyWayApp/app/src/main/java/com/example/odyway/ui/theme/Color.kt
+++ app/OdyWayApp/app/src/main/java/com/example/odyway/ui/theme/Color.kt
@@ -11,3 +11,11 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val BackgroundSplash = Color(0xFF172635)
+
+val NavyBlue = Color(0xFF0F1E2D)     // Fondo del compás / Textos oscuros
+val CyanBlue = Color(0xFF29B6F6)
+val GoldOrange = Color(0xFFF4A261)   // Camino dorado / Botones
+val MapPinRed = Color(0xFFE63946)    // Pin rojo / Selección
+val MountainGreen = Color(0xFF2A9D8F) // Montañas / Etiquetas
+val BackgroundLight = Color(0xFFF8F9FA) // Fondo general de la app
+val White = Color(0xFFFFFFFF) // Fondo general de la app
